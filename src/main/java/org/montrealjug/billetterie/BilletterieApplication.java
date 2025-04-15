ee51d3997f7e4eb1440ef0781649defa37459959
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-04-15 15:44:13 -0400

    feat(activity-form): allow editing maxParticipants and maxWaitingQueue in UI

--- src/main/java/org/montrealjug/billetterie/BilletterieApplication.java
+++ src/main/java/org/montrealjug/billetterie/BilletterieApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BilletterieApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BilletterieApplication.class, args);
 	}
 
