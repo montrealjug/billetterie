bb644195be3d51cb1f6532cc428f6f2c44d70e15
felissedano <contact@felissedano.com>
2025-04-18 20:35:58 -0400

    fix whitelabel error at index page when there is currently zero actvity created

--- src/main/java/org/montrealjug/billetterie/ui/IndexController.java
+++ src/main/java/org/montrealjug/billetterie/ui/IndexController.java
@@ -22,16 +22,16 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         Optional<Event> optionalEvent = this.eventRepository.findByActiveIsTrue();
-        PresentationEvent presentationEvent;
+
         if (optionalEvent.isPresent()) {
             Event event = optionalEvent.get();
-            presentationEvent = new PresentationEvent(event.getId(), event.getTitle(), event.getDescription(), event.getDate(), toIndexActivities(event.getActivities()), event.isActive());
-        } else {
-            throw new RuntimeException("No active event could be found!");
+            PresentationEvent presentationEvent = new PresentationEvent(event.getId(), event.getTitle(), event.getDescription(), event.getDate(), toIndexActivities(event.getActivities()), event.isActive());
+            model.addAttribute("event", presentationEvent);
         }
-        model.addAttribute("event", presentationEvent);
+
         return "index";
     }
 
 
 }
+
